a72f65b6f794ab2c309a88b603964ce5735ccf3c
Sam Alws <saalws@ctemc.org>
2019-03-23 23:01:49 -0400

    started to add scripts

--- placegen.lua
+++ placegen.lua
@@ -1,3 +1,5 @@
+require("lfs")
+
 function read(filename)
 	local file = io.open(filename, "r")
 	local val = file:read("*all")
@@ -7,9 +9,6 @@ end
 function readXml(filename)
 	return read("xmlSrc/"..filename..".xml")
 end
-function readLua(filename)
-	return read("luaSrc/"..filename..".lua")
-end
 function replaceComments(str, comments, replacements)
 	local str = str
 	for k, v in pairs(comments) do
@@ -17,9 +16,26 @@ function replaceComments(str, comments, replacements)
 	end
 	return str
 end
+function readLua(filename)
+	local val = read("luaSrc/"..filename..".lua")
+	return val
+end
+function replaceRequires(source)
+	local modified = source
+	local searchFor = "require(\""
+	local loc = 0
+	while true do
+		--TODO
+		loc += 1
+		if loc >= string.len(modified) - string.len(searchFor) then break end
+	end
+	return modified
+end
 
 local game = readXml("game")
 local service = readXml("service")
+local script = readXml("script")
+
 local toReplace = {"Lighting", "ReplicatedStorage", "ServerStorage", "StarterPlayer"}
 local replacements = {}
 for _, r in pairs(toReplace) do
@@ -30,11 +46,27 @@ for _, r in pairs(toReplace) do
 	))
 end
 
+local serverScriptService = ""
+for f in lfs.dir("luaSrc") do
+	if f ~= "." and f ~= ".." then
+		local strippedFilename = string.sub(f,0,-5)
+		local content = replaceRequires(read("luaSrc/"..f))
+		serverScriptService = serverScriptService..replaceComments(
+			script,
+			{"name", "content"},
+			{strippedFilename, content}
+		)
+	end
+end
+table.insert(toReplace, "ServerScriptService")
+table.insert(replacements, serverScriptService)
+
 local place = replaceComments(
 	readXml("game"),
 	toReplace,
 	replacements
 )
+
 local file = io.open("place.rbxlx", "w")
 file:write(place)
 file:close()
