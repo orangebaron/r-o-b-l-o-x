1ab46bf3989180bb00d6898f161746efb81e0da7
Sam Alws <saalws@ctemc.org>
2019-04-03 16:10:44 -0400

    basemap

--- luaSrc/genBaseMap.lua
+++ luaSrc/genBaseMap.lua
@@ -1,9 +1,9 @@
 return function()
 	local model = Instance.new("Model")
-	model.Name = "SpectatorArea"
+	model.Name = "MapBase"
 	local part = Instance.new("Part", model)
 	part.Anchored = true
-	part.Position = Vector3.new(-2, -1, 0)
+	part.Position = Vector3.new(2, -1, 0)
 	part.Size = Vector3.new(4, 2, 4)
 	part.BrickColor = BrickColor.Green()
 
