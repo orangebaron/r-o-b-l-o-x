7ee9650fd7a2867968f46140a8b9f719f26b57b0
Sam Alws <saalws@ctemc.org>
2019-04-03 16:39:09 -0400

    remove base map thing

--- luaSrc/initialSetup.lua
+++ luaSrc/initialSetup.lua
@@ -1,9 +1,7 @@
 require("genSpectatorArea")
-require("genBaseMap")
 require("playerAdded")
 
 return function()
 	genSpectatorArea().Parent = Workspace
-	genBaseMap().Parent = Workspace
 	game:GetService("Players").PlayerAdded:connect(playerAdded)
 end
